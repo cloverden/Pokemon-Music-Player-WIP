f18977c162574406c8f65a51777f5029e89b2111
Patricia Ceban <patricia.ceban.contact@gmail.com>
2025-05-25 00:22:12 -0400

    fixed page bounce

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = 4001;
 
-// Serve static files (HTML, CSS, JavaScript)
+//for serving static files (HTML, CSS, JavaScript)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the home page
+//for routing home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'main.html'));
 });
 
-// Start the server
+//starting the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
